a44d27893012624201bdbd5b9dab038cb53add75
unknown <ojitdebnathfreelancer.com>
2023-01-13 11:13:51 +0600

    dlkfj

--- src/Pages/Impact/Direct/Direct.jsx
+++ src/Pages/Impact/Direct/Direct.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Direct = () => {
     return (
         <div>
-            <h1>This is direct page</h1>
+            <h1>This is direct  pages</h1>
         </div>
     );
 };
