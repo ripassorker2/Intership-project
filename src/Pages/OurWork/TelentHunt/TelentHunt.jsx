e29c60721ff1fde9fdbd3cdeff7c2709ac9a5fc1
arifulgony <arifulgony@gmail.com>
2023-01-24 21:55:14 +0600

    event

--- src/Pages/OurWork/TelentHunt/TelentHunt.jsx
+++ src/Pages/OurWork/TelentHunt/TelentHunt.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Title from '../../../Shared/Title/Title';
-
+import img1 from "../../../assets/event/Event.jpg";
+import img2 from "../../../assets/event/event-2.jpg";
+import img3 from "../../../assets/event/event-3.jpg";
 
 const TelentHunt = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="max-w-screen-xl mx-auto my-20">
       <Title title={'Telent Hunt'}></Title>
       <div className="hero-content text-center text-neutral-content">
         <p className="text-black mb-5">
@@ -15,6 +18,33 @@ const TelentHunt = () => {
           winners and participants with tokens of appreciation.
         </p>
       </div>
+      <div className=" gap-10 justify-center mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+          <div className="">
+            <div className=" card-compact w-96 bg-base-100 shadow-xl ">
+              <figure><img className='h-[420px]' src={img1} alt="Shoes" /></figure>
+            </div>
+          </div>
+          <div className="">
+           <Link to="/august19">
+           <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure><img className='h-[350px]' src={img2} alt="Shoes" /></figure>
+              <div className="card-body bg-[#F48E40] hover:bg-[#fff] ">
+                <h2 className="card-title">AUGUST 15-2019</h2>
+              </div>
+            </div>
+           </Link>
+          </div>
+          <div className="">
+            <Link to="/august20">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure><img className='h-[350px] w-96' src={img3} alt="Shoes" /></figure>
+              <div className="card-body  bg-[#F48E40] hover:bg-[#fff]">
+                <h2 className="card-title">AUGUST 15 - 2020</h2>
+              </div>
+            </div>
+            </Link>
+          </div>
+          </div> 
     </div>
   );
 };
--- src/Pages/Partners/Events/Events.jsx
+++ src/Pages/Partners/Events/Events.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import img1 from "../../../assets/event/Event.jpg";
-import img2 from "../../../assets/event/event-2.jpg";
-import img3 from "../../../assets/event/event-3.jpg";
 import demo from "../../../assets/event/event-0 (2).jpg";
 //import { AiFillFolderOpen } from "react-icons/ai";
 
@@ -12,33 +9,6 @@ const Events = () => {
       <div className="max-w-screen-xl mx-auto my-20">
         <h1 className="text-5xl font-bold text-center">EVENTS</h1>
         <div className=" gap-10 justify-center mt-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-          <div className="">
-            <div className="card card-compact w-96 bg-base-100 shadow-xl ">
-              <figure><img className='h-[420px]' src={img1} alt="Shoes" /></figure>
-             
-            </div>
-          </div>
-          <div className="">
-           <Link to="/august19">
-           <div className="card card-compact w-96 bg-base-100 shadow-xl">
-              <figure><img className='h-[350px]' src={img2} alt="Shoes" /></figure>
-              <div className="card-body bg-[#F48E40] hover:bg-[#fff] ">
-                <h2 className="card-title">AUGUST 15-2019</h2>
-              </div>
-            </div>
-           </Link>
-          </div>
-          <div className="">
-            <Link to="/august20">
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-              <figure><img className='h-[350px] w-96' src={img3} alt="Shoes" /></figure>
-              <div className="card-body  bg-[#F48E40] hover:bg-[#fff]">
-                <h2 className="card-title">AUGUST 15 - 2020</h2>
-              </div>
-            </div>
-            </Link>
-          </div>
- 
                   <Link to="/AwardsCeremony">
                   <div className="card card-compact w-96 bg-base-100 shadow-xl">
                       <figure><img className='h-[350px] w-96' src={demo} alt="Shoes" /></figure>
