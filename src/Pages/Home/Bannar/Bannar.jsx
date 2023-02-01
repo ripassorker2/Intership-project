28118b27cea55404b526d16480e1be7880e2b4d4
shuvornoy <shuvornoy@gmaol.com>
2023-02-01 19:01:15 +0600

    update banner

--- src/Pages/Home/Bannar/Bannar.jsx
+++ src/Pages/Home/Bannar/Bannar.jsx
@@ -1,21 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 import "./Banner.css";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import BannerItem from "./BannerItem";
+import img1 from '../../../assets/banner/1 (Custom).jpg'
+import img2 from '../../../assets/banner/2 (Custom).jpg'
+import img3 from '../../../assets/banner/3 (Custom).jpg'
+import img4 from '../../../assets/banner/4 (Custom).jpg'
+import img5 from '../../../assets/banner/6 (Custom).jpg'
+import img6 from '../../../assets/banner/7 (Custom).jpg'
 
 const Banner = () => {
-  const [banner, setBanner] = useState([]);
+   const banner = [
+     {
+       id: 1,
+       img: img1,
+     },
+     {
+       id: 2,
+       img: img2,
+     },
+     {
+       id: 3,
+       img: img3,
+     },
+     {
+       id: 4,
+       img: img4,
+     },
+     {
+       id: 5,
+       img: img5,
+     },
+     {
+       id: 6,
+       img: img6,
+     },
+   ];
+  // const [banner, setBanner] = useState([]);
 
-  useEffect(() => {
-    fetch("banner.json")
-      .then((res) => res.json())
-      .then((data) => setBanner(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("banner.json")
+  //     .then((res) => res.json())
+  //     .then((data) => setBanner(data));
+  // }, []);
   return (
     <div>
       <Swiper
@@ -32,7 +63,8 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper xl:mt-[122px] lg:mt-[112px] md:mt-[92px] mt-[82px]"
       >
-        {banner.map((banner, i) => (
+        {
+        banner.map((banner, i) => (
           <SwiperSlide key={i}>
             <BannerItem banner={banner}></BannerItem>
           </SwiperSlide>
--- src/Pages/Home/Bannar/BannerItem.jsx
+++ src/Pages/Home/Bannar/BannerItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import img from '../../../assets/banner/banner-half-motiff.svg'
+import sitePhoto from '../../../assets/banner/banner-half-motiff.svg'
 
 
 const BannerItem = ({banner}) => {
-   const { picture } = banner;
+   const { img } = banner;
    return (
      <>
        <div className="banner_slide w-[100%]">
@@ -12,14 +12,14 @@ const BannerItem = ({banner}) => {
              className="banner_right xl:w-[480px] lg:w-[340px] 
              md:w-[250px] hidden sm:block"
              data-aos="fade-down-left"
-             src={img}
+             src={sitePhoto}
              alt=""
            />
          </div>
          <div className="slide-img">
            <img
              className="slider_img relative w-full h-full"
-             src={picture}
+             src={img}
              alt="img"
            />
          </div>
